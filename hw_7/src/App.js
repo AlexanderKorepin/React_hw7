86f77b1fb59fb5421fdda9ccbffdb9299da94554
AlexanderKorepin <1987kerykery@mail.ru>
2024-11-17 20:14:08 +0200

    hw_7

---
+++ hw_7/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import TasksList from "./components/TasksList";
+
+function App() {
+  return (
+    <div>
+      <TasksList />
+    </div>
+  );
+}
+
+export default App;
